6a77cf7738e8ea53e51a6a84496ffc5ce020b1d7
André Campher <ahcampher@tuks.co.za>
2010-04-29 14:31:01 +0200

    * Initial phase - solving linear equations * Function to generate qhull data-file needs to be separated

--- con2vert.py
+++ con2vert.py
@@ -6,4 +6,67 @@
 # Converts sets of constraints to a list of vertices (of the feasible region)
 #
 # Author: Michael Kelder (Original)
-#		  André Campher (Python implementation)
+#         Andre Campher (Python implementation)
+
+# ===== Original MATLAB Code =====
+#c = A\b;
+#if ~all(A*c < b);
+#    [c,f,ef] = fminsearch(@obj,c,'params',{A,b});
+#    if ef ~= 1
+#        error('Unable to locate a point within the interior of a feasible region.')
+#    end
+#end
+#b = b - A*c;
+#D = A ./ repmat(b,[1 size(A,2)]);
+#[k,v2] = convhulln([D;zeros(1,size(D,2))]);
+#[k,v1] = convhulln(D);
+#if v2 > v1
+#    error('Non-bounding constraints detected. (Consider box constraints on variables.)')
+#end
+#nr = unique(k(:));
+#G  = zeros(size(k,1),size(D,2));
+#for ix = 1:size(k,1)
+#    F = D(k(ix,:),:);
+#    G(ix,:)=F\ones(size(F,1),1);
+#end
+#V = G + repmat(c',[size(G,1),1]);
+#[null,I]=unique(num2str(V,6),'rows');
+#V=V(I,:);
+#return
+#function d = obj(c,params)
+#A=params{1};
+#b=params{2};
+#d = A*c-b;
+#k=(d>=-1e-15);
+#d(k)=d(k)+1;
+#d = max([0;d]);
+#return
+
+# Dependencies : - Scipy
+#		 - Numpy
+
+from scipy import *
+from numpy import linalg, matlib
+
+#unit cube in 3D for testing
+A = mat('-1 0 0; 0 -1 0; 0 0 -1; 1 0 0; 0 1 0; 0 0 1')
+b = mat('0;0;0;1;1;1')
+
+c = linalg.lstsq(A,b)[0]
+b = b-A*c
+D = A / matlib.repmat(b,1,A.shape[1])
+
+#qhullp = subprocess.Popen('qhull n < qhullin', shell=True, stdout=subprocess.PIPE) #calc convex hull and get normals
+#Vc = qhullp.communicate()[0] #qhull output to Vc
+#ks = Vc.split('\n')
+#ks = string.join(ks[2:],';') #remove leading dimension output
+#k = mat(ks[:-1])
+
+#if v2 > v1
+#    error('Non-bounding constraints detected. (Consider box constraints on variables.)')
+#end
+
+print D
+
+#TODO =====
+# error-checking
