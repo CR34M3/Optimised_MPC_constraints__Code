0d3040f1ea00fd912a4e2c1746e91dd061b0d988
André Campher <ahcampher@tuks.co.za>
2010-05-10 13:49:45 +0200

    - changed main to used vert2con and con2vert as functions - calculated intersection AOS|DOS and expressed as constraints

--- main.py
+++ main.py
@@ -9,43 +9,59 @@
 # Dependencies: SciPy
 
 from scipy import *
+from con2vert import con2vert
+from vert2con import vert2con
+
+def mat2ab(Asbmat): 	# transform to standard Ax<b notation with sign vector
+	A = Asbmat[:,:-2]
+	s = Asbmat[:,-2]
+	b = Asbmat[:,-1]
+	A = multiply(A,-s)
+	b = multiply(b,-s)
+	return A,s,b
+
+#===== MAIN START =====
 
 # insert AIS (equations : Ax<b)
-#	- convert to vertices on feasible region
-#   - con2vert [translation in progress]
-AIS = mat('1 0 1 -1; 1 0 -1 1; 0 1 1 -1; 0 1 -1 1') #equations in the form Ax<b, matrix = [A s b] with s the sign vector
-#AIS_s =
-#AIS_A =
-#AIS_b =  
-AISvert = mat('-1 -1; -1 1; 1 -1; 1 1') #manual vertex insertion while con2vert is being translated
-
+AIS = mat('1 0  1 -1;\
+	   1 0 -1  1;\
+	   0 1  1 -1;\
+	   0 1 -1  1') #equations in the form Ax<b, matrix = [A s b] with s the sign vector [1:>, -1:<]
 # insert DOS (equations : Ax<b)
-#	- convert to vertices on feasible region (necessary?)
-DOS = mat('1 0 1 -1; 1 0 -1 1; 0 1 1 -1; 0 1 -1 1') #equations in the form Ax<b, matrix = [A s b] with s the sign vector
-#DOS_s =
-#DOS_A =
-#DOS_b =  
-DOSvert = mat('-1 -1; -1 1; 1 -1; 1 1') #manual vertex insertion while con2vert is being translated
-tester = mat('1; 1')
-
+DOS = mat('1 0  1 -1;\
+	   1 0 -1  1;\
+	   0 1  1 -1;\
+	   0 1 -1  1') #equations in the form Ax<b, matrix = [A s b] with s the sign vector [1:>, -1:<]
 # insert G (steady-state model)
 G = mat('1.2 0.5; 0.5 1.2') #gain matrix - atm linear steady state matrix
 
+
+# convert to A,b form
+AIS_A,AIS_s,AIS_b = mat2ab(AIS)
+DOS_A,DOS_s,DOS_b = mat2ab(DOS)
+
+# convert to vertices on feasible region
+AISvert = con2vert(AIS_A,AIS_b)
+DOSvert = con2vert(DOS_A,DOS_b)
+
 # calc AOS (from G and AIS)
-#	- convert vertices to equations (Ax<b)
-#   - vert2con [translation in progress]
 AOSverttemp = empty([1,AISvert.shape[1]])
 for vert in AISvert:
 	x = G*vert.transpose()
 	AOSverttemp = vstack((AOSverttemp,x.transpose()))
-
 AOSvert = AOSverttemp[1:,:] #remove first line of junk data from AOSverttemp
 
-print AOSvert
+# convert AOS vertices to equations (Ax<b)
+AOS_A,AOS_s,AOS_b = vert2con(AOSvert)
 
 # Calc intersection of AOS|DOS
-#	- use ver2con on AOSvert
-#	- use con2vert with AOScon and DOScon to get intersection
+#	- using con2vert with both AOS constraints and DOS constraints should result in the intersection
+combA = vstack((AOS_A,DOS_A))
+combb = vstack((AOS_b,DOS_b))
+intAOSDOSvert = con2vert(combA,combb)
+intAOSDOScon = vert2con(intAOSDOSvert)
+
+print intAOSDOScon
 
 #TODO
 # (Vectors defining inside/outside to calc)
