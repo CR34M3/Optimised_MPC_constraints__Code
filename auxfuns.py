1df375fcd27814036b0a1af88a955d027c38e1c8
André Campher <ahcampher@tuks.co.za>
2010-05-13 17:55:17 +0200

    * Fixed problem in code ('all' should be 'any')

--- auxfuns.py
+++ auxfuns.py
@@ -6,13 +6,13 @@ import numpy
 # uniqm ====================================================================================================
 # Removes duplicate entries from a matrix
 #    INPUT:  A - matrix (possibly containing duplicates)
-#        t - tolerance (float) [positive]
+#            t - tolerance (float) [positive]
 #    OUTPUT: matrix (A with the duplicates removed)
 def uniqm(A,t):
     Nrows = A.shape[0]
     uniquerows = [r1 for r1 in range(Nrows)
-              if not all(numpy.all(abs(A[r1,:]-A[r2,:])<t)
-                 for r2 in range(r1+1,Nrows))] 
+              if not any(numpy.all(abs(A[r1,:]-A[r2,:])<t)
+             for r2 in range(r1+1,Nrows))]
     return A[uniquerows,:].copy()
 
 
