24bd5159c624214f6579877e8e4bda4ee656398e
Andre <andre@CR34M3-LAP.(none)>
2010-07-14 14:30:35 +0200

    * [added] methods for output constraints and for intersections

--- conclasses.py
+++ conclasses.py
@@ -6,4 +6,29 @@ Author: Andre Campher
 # Dependencies: - convertfuns
 #               - auxfuns
 
+from auxfuns import mat2ab
+from convertfuns import *
 
+class conset:
+    def __init__(self,conmatrix):
+        asb = mat2ab(conmatrix)
+        self.A = asb[0]
+        self.s = asb[1]
+        self.b = asb[2]
+        self.vert = con2vert(self.A,self.b)
+        
+    def outconlin(self,model):
+        """Convert constraints to ouput space using a linear model"""       
+        # calc AOS (from G and AIS)
+        verttemp = empty([1,self.vert.shape[1]])
+        for v in self.vert:
+            x = model*v.transpose()
+            outverttemp = vstack((outverttemp,x.transpose()))
+        return outverttemp[1:,:] #remove first line of junk data from outverttemp
+    
+    def intersect(self,conset2):
+        """Determine intersection between current constraint set and another"""
+        combA = vstack((self.A,conset2.A))
+        combb = vstack((self.b,conset2.b))
+        intcombvert = con2vert(combA,combb)
+        return vert2con(intcombvert)        
