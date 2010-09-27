23226e4f94d7ea0a630866db3e1b3130d785e528
Andre <andre@CR34M3-LAP.(none)>
2010-09-27 15:50:35 +0200

    - BRANCH dev-fitshape-faces-brute
    - [implementing] scipy.optimize.brute
    
    	Major error handling to be done

--- fitting.py
+++ fitting.py
@@ -34,45 +34,6 @@ def splitAb(inAb, nd):
     b = array([tmpAb[:, -1]]).T
     return A, b
 
-def genstart(ncon, cs):
-    """
-    Generate a starting shape (for optimisation) with given number of faces.
-    """
-    #1. Determine centre of initial region, cscent
-    cscent = sum(cs.vert)/len(cs.vert)  # assuming the region is convex
-    #2. Generate ncon-1 Gaussian vectors
-    spherevecs = ones((ncon, cs.vert.shape[1]))
-    for rows in range(spherevecs.shape[0] - 1):
-        for cols in range(spherevecs.shape[1]):
-            spherevecs[rows, cols] = random.gauss(0, 0.33)  # TODO: check sigma
-    #3. Determine resultant of vectors, add last vector as mirror of resultant
-    spherevecs[-1, :] = -sum(spherevecs[:-1, :])
-    # points on sphere
-    def spherefn(srad):
-        """
-        Function to create points on a sphere (of radius, srad), convert them to
-        halfspaces and convert the halfspaces to a constraint set.
-        """
-        spherepts = spherevecs.T*(srad/sqrt(sum((spherevecs.T)**2)))  
-        spherepts = spherepts.T + cscent  # move to center of constraint set
-        #4. Generate tangent planes on sphere at points, convert to inequalities
-        A = -(spherepts - cscent)
-        b = array([(sum((A*spherepts).T))]).T
-        s = array(ones(b.shape))
-        return ConSet(A, s, b)  # constraints around sphere
-    
-    #6. Optimise sphere-radius, r, to have all points within initial shape
-    startrad = 1.0
-    if spherefn(startrad).allinside(cs)[0]:
-        while spherefn(startrad).allinside(cs)[0]:
-            startrad = startrad * 1.1
-        finrad = startrad / 1.1    
-    elif not spherefn(startrad).allinside(cs)[0]:
-        while not spherefn(startrad).allinside(cs)[0]:
-            startrad = startrad / 1.1
-        finrad = startrad
-    return spherefn(finrad)
-
 def fitshape(ncon, cset):
     """
     Fit a constraint set (specified by the number of constraints) within an existing
@@ -84,53 +45,35 @@ def fitshape(ncon, cset):
     # ncongiven > ncon >= nD+1
     # Constraints are bounding
     # All vertices within constraint set
-    #### Define parameters
-    spset = genstart(ncon, cset) 
-    #spset = ConSet(array([[1., 1], [1, 9], [9, 1]]))
-    sp = c_[spset.A, spset.b] # starting point - combined Ab matrix to optimise
-    vp2 = vstack([spset.vert, spset.vert[0, :]])
-    plot(vp2[:, 0], vp2[:, 1], 'g')
     
     #### Objective fn
     def objfn(Ab, *args):
         """Volume objective function."""
         initcs = args[0]
         A, b = splitAb(Ab, initcs.nd)
-        cl = con2vert(A, b)[1]
+        try:
+            cl = con2vert(A, b)[1]
+        except NameError:
+            cl = -1
         if cl:
             closed = 1
         else:
             closed = -1
         vol = tryvol(A, b, initcs)[0]
-#        print c_[A, -ones(b.shape), b]
-#        print closed * -vol
-#        raw_input("Key...")
         if not cl:
             print vol
         return closed * -vol
-    #### Constraints
-    def eqconsfn(Ab, *args):
-        """Optimiser equality constraint function."""
-        initcs = args[0]
-        b = splitAb(Ab, initcs.nd)[1]
-        return array([linalg.norm(b) - 10])
-    def ieqconsfn(Ab, *args):
-        """Optimiser inequality constraint function."""
-        initcs = args[0]
-        A, b = splitAb(Ab, initcs.nd)
-        # get vertices for constraint set iteration
-        V = tryvol(A, b, initcs)[1]
-        iterset = ConSet(V)
-        #constraint checking for vertices
-        ineqs = iterset.allinside(initcs)[1]
-        return ineqs.ravel()
+    #### Constraints (generate ranges)
+    Abranges = ((-1, 1), (-1, 1), (-1, 1), (-1, 1), (-1, 1), (-1, 1), (-1, 1), (-1, 1), (-1, 1))
+#    for k in range(9-1):
+#        Abranges = Abranges, (-1, 1)
     #### Maximise volume
-    optAb = optimize.fmin_slsqp(objfn, sp, f_eqcons=eqconsfn, 
-                               f_ieqcons=ieqconsfn, args=[cset], iprint=3)
+    optAb = optimize.brute(objfn, Abranges, args=[cset], Ns=3, finish=None)
     return optAb
 
 if __name__ == "__main__":
     from pylab import plot, show
+# ===== TEST 1
     v = array([[0, 0], [10, 0], [10, 10], [0, 10]])
     initcset = ConSet(v)
     Abopt = fitshape(3, initcset)
@@ -144,3 +87,12 @@ if __name__ == "__main__":
     vst = array([[1, 1], [1, 9], [9, 1], [1, 1]])
     plot(vp[:, 0], vp[:, 1], 'b', vp2[:, 0], vp2[:, 1], 'r')
     show()
+# ======= TEST 2
+#    from auxfuns import mat2ab
+#    AISA, AISs, AISb = mat2ab(array([[1., 0., 1., 0.],
+#                                     [0., 1., 1., 0.],
+#                                     [0.5, 0.5, -1, 1.]]))
+#    AIS = ConSet(AISA, AISs, AISb)
+#    vp2 = vstack([AIS.vert, AIS.vert[0, :]])
+#    plot(vp2[:, 0], vp2[:, 1], 'r')
+#    show()
